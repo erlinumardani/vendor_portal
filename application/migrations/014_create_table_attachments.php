ba7f71e9d492c2df124025ff7f537a92f5198e3c
Erlin Umar Dani <erlinumardani@gmail.com>
2020-02-24 00:18:02 +0700

    add attachments

---
+++ application/migrations/014_create_table_attachments.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @author   Natan Felles <natanfelles@gmail.com>
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Class Migration_create_table_users
+ *
+ * @property CI_DB_forge         $dbforge
+ * @property CI_DB_query_builder $db
+ */
+class Migration_create_table_attachments extends CI_Migration {
+
+
+	protected $table = 'attachments';
+
+
+	public function up()
+	{
+		$fields = array(
+			'id' => [
+				'type' => 'INT(11)',
+				'auto_increment' => TRUE
+			],
+			'flow_request_id'=> [
+				'type' => 'INT(11)'
+			],
+			'name'=> [
+				'type' => 'VARCHAR(50)'
+			],
+			'url'=> [
+				'type' => 'TEXT'
+			],
+			'updated_by' => [
+				'type' => 'VARCHAR(20)',
+				'null' => TRUE,
+				'unsigned' => TRUE,
+				'default' => 'System'
+			],
+			'created_at' => [
+				'type' => 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP' 
+			],
+			'updated_at' => [
+				'type' => 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+				'null' => TRUE
+			]
+		);
+		$this->dbforge->add_field($fields);
+		$this->dbforge->add_key('id', TRUE);
+		$this->dbforge->create_table($this->table, TRUE);
+
+	}
+
+	public function down()
+	{
+		if ($this->db->table_exists($this->table))
+		{
+			$this->dbforge->drop_table($this->table);
+		}
+	}
+
+}
