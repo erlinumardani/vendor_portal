176391318d8d97cf9fff0c1137ae9bcaf11ec820
Erlin Umar Dani <erlinumardani@gmail.com>
2020-02-24 13:20:25 +0700

    add view v_request_logs

--- application/migrations/015_create_table_process_flow_request_logs.php
+++ application/migrations/015_create_table_process_flow_request_logs.php
@@ -61,6 +61,20 @@ class Migration_create_table_process_flow_request_logs extends CI_Migration {
 		$this->dbforge->add_key('id', TRUE);
 		$this->dbforge->create_table($this->table, TRUE);
 
+		//create view
+		$this->db->query(
+			"CREATE 
+				OR REPLACE VIEW `v_request_logs` AS SELECT
+				`d`.`fullname` AS `updater`,
+				`b`.`name` AS `flow_node_name`,
+				TIMESTAMPDIFF(MINUTE,`a`.`start`,`a`.`end`) AS duration,
+				`a`.* 
+			FROM
+				`process_flow_request_logs` AS `a`
+				LEFT JOIN `process_flow_nodes` AS `b` ON `b`.`id` = `a`.`flow_node_id`
+				LEFT JOIN `users` AS `c` ON `c`.`id` = `a`.`updated_by`
+				LEFT JOIN `persons` AS `d` ON `d`.`id` = c.`person_id`"
+		);
 	}
 
 
